9dd9bde9b6c0690e68963868766fe2c9cda124bc
bang0392 <145949937+bang0392@users.noreply.github.com>
2025-04-27 17:07:50 +0700

    1st

---
+++ CompareImages/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace CompareImages
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            btnChooseImages = new Button();
+            btnCompare = new Button();
+            lblResult = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(984, 188);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(12, 332);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(984, 261);
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // btnChooseImages
+            // 
+            btnChooseImages.Location = new Point(233, 234);
+            btnChooseImages.Name = "btnChooseImages";
+            btnChooseImages.Size = new Size(111, 49);
+            btnChooseImages.TabIndex = 2;
+            btnChooseImages.Text = "Chọn ảnh";
+            btnChooseImages.UseVisualStyleBackColor = true;
+            btnChooseImages.Click += btnChooseImages_Click;
+            // 
+            // btnCompare
+            // 
+            btnCompare.Location = new Point(440, 234);
+            btnCompare.Name = "btnCompare";
+            btnCompare.Size = new Size(111, 49);
+            btnCompare.TabIndex = 3;
+            btnCompare.Text = "So Sánh";
+            btnCompare.UseVisualStyleBackColor = true;
+            btnCompare.Click += btnCompare_Click;
+            // 
+            // lblResult
+            // 
+            lblResult.Location = new Point(621, 251);
+            lblResult.Name = "lblResult";
+            lblResult.Size = new Size(167, 23);
+            lblResult.TabIndex = 4;
+            lblResult.Text = ".....";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1056, 637);
+            Controls.Add(lblResult);
+            Controls.Add(btnCompare);
+            Controls.Add(btnChooseImages);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Button btnChooseImages;
+        private Button btnCompare;
+        private Label lblResult;
+    }
+}
---
+++ CompareImages/Form1.cs
@@ -0,0 +1,163 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using OpenCvSharp.Features2D;
+
+namespace CompareImages
+{
+    public partial class Form1 : Form
+    {
+        private string imagePath1, imagePath2;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChooseImages_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "Image Files|*.jpg;*.png;*.bmp";
+                ofd.Multiselect = true;
+
+                if (ofd.ShowDialog() == DialogResult.OK && ofd.FileNames.Length == 2)
+                {
+                    imagePath1 = ofd.FileNames[0];
+                    imagePath2 = ofd.FileNames[1];
+
+                    pictureBox1.Image = new System.Drawing.Bitmap(imagePath1);
+                    pictureBox2.Image = new System.Drawing.Bitmap(imagePath2);
+                }
+                else
+                {
+                    MessageBox.Show("Vui lòng chọn đúng 2 ảnh!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+
+        private void btnCompare_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(imagePath1) || string.IsNullOrEmpty(imagePath2))
+            {
+                MessageBox.Show("Hãy chọn 2 ảnh trước!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            Mat resultImage = CompareAndHighlightDifferences(imagePath2, imagePath1);
+            Mat resultImage1 = CompareAndHighlightDifferences(imagePath1, imagePath2);
+
+            if (resultImage != null)
+            {
+                pictureBox1.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(resultImage1);
+                pictureBox2.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(resultImage);
+                lblResult.Text = "Kết quả: Có sự khác biệt!";
+            }
+            else
+            {
+                lblResult.Text = "Kết quả: Ảnh giống nhau!";
+            }
+        }
+
+        private Mat CompareAndHighlightDifferences(string imgPath1, string imgPath2)
+        {
+            using var img1 = Cv2.ImRead(imgPath1, ImreadModes.Color);
+            using var img2 = Cv2.ImRead(imgPath2, ImreadModes.Color);
+
+            if (img1.Empty() || img2.Empty())
+                throw new Exception("Không đọc được ảnh.");
+
+            // Resize nhỏ lại nếu ảnh quá lớn
+            if (img1.Width > 1000)
+                Cv2.Resize(img1, img1, new OpenCvSharp.Size(800, img1.Height * 800 / img1.Width));
+            if (img2.Width > 1000)
+                Cv2.Resize(img2, img2, new OpenCvSharp.Size(800, img2.Height * 800 / img2.Width));
+
+            // Chuyển về grayscale
+            using var gray1 = new Mat();
+            using var gray2 = new Mat();
+            Cv2.CvtColor(img1, gray1, ColorConversionCodes.BGR2GRAY);
+            Cv2.CvtColor(img2, gray2, ColorConversionCodes.BGR2GRAY);
+
+            // Tạo đối tượng SIFT
+            var sift = SIFT.Create();
+
+            // Tìm keypoints và descriptors
+            KeyPoint[] keypoints1, keypoints2;
+            Mat descriptors1 = new Mat(), descriptors2 = new Mat();
+            sift.DetectAndCompute(gray1, null, out keypoints1, descriptors1);
+            sift.DetectAndCompute(gray2, null, out keypoints2, descriptors2);
+
+            // Dùng BFMatcher với L2 norm
+            var bf = new BFMatcher(NormTypes.L2, crossCheck: true);
+            var matches = bf.Match(descriptors1, descriptors2);
+
+            if (matches.Length < 4)
+                return null; // Không đủ điểm để align
+
+            // Lấy các match tốt nhất
+            matches = matches.OrderBy(m => m.Distance).Take(50).ToArray();
+
+            var srcPoints = matches.Select(m => keypoints1[m.QueryIdx].Pt).ToArray();
+            var dstPoints = matches.Select(m => keypoints2[m.TrainIdx].Pt).ToArray();
+
+            // Tạo Mat từ các điểm
+            Mat srcMat = new Mat(srcPoints.Length, 1, MatType.CV_32FC2);
+            Mat dstMat = new Mat(dstPoints.Length, 1, MatType.CV_32FC2);
+
+            // Chuyển các điểm thành Point2f và gán vào Mat
+            for (int i = 0; i < srcPoints.Length; i++)
+            {
+                srcMat.Set(i, 0, new Point2f((float)srcPoints[i].X, (float)srcPoints[i].Y));
+                dstMat.Set(i, 0, new Point2f((float)dstPoints[i].X, (float)dstPoints[i].Y));
+            }
+
+            // Tính toán Homography
+            var homography = Cv2.FindHomography(srcMat, dstMat, HomographyMethods.Ransac);
+
+            if (homography.Empty())
+                return null;
+
+            // Warp img2 về cùng góc nhìn với img1
+            using var alignedImg2 = new Mat();
+            Cv2.WarpPerspective(img2, alignedImg2, homography, img1.Size());
+
+            // Bây giờ mới so sánh pixel
+            using var diff = new Mat();
+            Cv2.Absdiff(img1, alignedImg2, diff);
+
+            using var diffGray = new Mat();
+            Cv2.CvtColor(diff, diffGray, ColorConversionCodes.BGR2GRAY);
+
+            using var thresh = new Mat();
+            Cv2.Threshold(diffGray, thresh, 30, 255, ThresholdTypes.Binary);
+
+            Cv2.FindContours(thresh, out OpenCvSharp.Point[][] contours, out _, RetrievalModes.External, ContourApproximationModes.ApproxSimple);
+
+            if (contours.Length == 0)
+                return null; // Không thấy sự khác biệt
+
+            var resultImg1 = img1.Clone();
+            var resultImg2 = alignedImg2.Clone();
+
+            // Vẽ khoanh tròn trên ảnh img1 và img2 mà không thay đổi ảnh gốc
+            foreach (var contour in contours)
+            {
+                var rect = Cv2.BoundingRect(contour);
+                var center = new OpenCvSharp.Point(rect.X + rect.Width / 2, rect.Y + rect.Height / 2);
+                int radius = Math.Max(rect.Width, rect.Height) / 2 + 10;
+
+                // Vẽ khoanh tròn đỏ trên ảnh 1 (để hiển thị sự khác biệt trên ảnh 1)
+                Cv2.Circle(resultImg1, center, radius, new Scalar(0, 0, 255), 3);  // Red circle on image 1
+
+                // Vẽ khoanh tròn xanh trên ảnh 2 (để phân biệt với ảnh 1)
+                Cv2.Circle(resultImg2, center, radius, new Scalar(255, 0, 0), 3);  // Blue circle on image 2
+            }
+
+            // Kết hợp ảnh img1 và img2 để hiển thị kết quả
+            var result = new Mat();
+            Cv2.HConcat(new Mat[] { resultImg1, resultImg2 }, result);
+
+            return result;
+        }
+
+    }
+}
